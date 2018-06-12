ad287d2ceffc1255f3a561e9ef05e06ad9b49b3f
Drayton80 <drayton80@hotmail.com>
2018-06-12 11:37:16 -0300

    v0.3 - Pre-processing (Corrections) and Files Reorganization

---
+++ pre-processing.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import numpy  as np
+
+# FUNÇÕES IMPORTADAS #==========================================================================================================#
+'''
+' Função criada por Douglas de Lira Lima (douglasliralima) em conjunto com Armando Neto (ArmandoTGT)
+'''
+def atributoNeutro(df):
+	new_df = pd.DataFrame(df)
+	#print(new_df['LvL Adjustment'].tail())
+	for atributo in new_df:
+		#print(atributo)
+		i = 0		
+		if(atributo == 'state'):
+			aux = new_df[atributo].tolist() 
+			for elemento in aux:
+				# print(i,elemento)
+				if(elemento == "suspended"):
+					new_df.drop(i, inplace = True)				
+						
+				if(elemento == "undefined"):
+					new_df.drop(i, inplace = True)				
+						
+				if(elemento == "canceled"):
+					new_df.drop(i, inplace = True)	
+
+				if(elemento == "failed"):
+					new_df.drop(i, inplace = True)			
+				i+=1			
+	#print(new_df['LvL Adjustment'].tail())
+	return new_df
+
+#====================#==========================================================================================================#
+
+# DEFINIÇÂO DAS FUNÇÕES INTERNAS #==============================================================================================#
+'''
+' Função Convert to Days:
+'   Descrição: Recebe uma data no formato ano-mês-dia e retorna ela num formato apenas em número de dias, contando
+'    apenas os anos DC, ou seja, começando sempre com o ano 1 como primeiro de todos
+'''
+def convertToDays(date):
+	#|print("Ano:", int(date[0:4]))
+	# Soma-se os dias de todos os anos anteriores [ ( int(date[0:4])-1 )*365 ], contando com à soma dos dias à 
+	# mais advindos dos anos bissextos anteriores [ int( (int(date[0:4])-1)/4) ], até chegar no ano da data e 
+	# verificar quantos dias em específico ele têm indo para os meses e, por fim, os dias
+	days = 0 + ( int(date[0:4])-1 )*365 + int( (int(date[0:4])-1)/4)
+
+	#|print("Dias após anos:", days)
+
+	# Se for maior que o mês significa que o número de dias do respectivo mês deve ser
+	# acrescentado na conta dos dias, ou seja, a cada mês passado seu número de dias deve ser
+	# incrementado na contagem de dias até que chegue no mês que o atributo da data está para só assim
+	# poder passar para a parte dos dias e somá-los na conta
+	# somar com os dos meses posteriores
+	#|print("Mês:", int(date[5:7]) )
+	if 1 < int(date[5:7]):
+		days += 31
+	if 2 < int(date[5:7]):
+		# Se o ano for bissexto Fevereiro terá 29 dias
+		if (int(date[0:4])%4) == 0:
+			days += 29
+		else:
+			days += 28
+	if 3 < int(date[5:7]):
+		days += 31
+	if 4 < int(date[5:7]):
+		days += 30
+	if 5 < int(date[5:7]):
+		days += 31
+	if 6 < int(date[5:7]):
+		days += 30
+	if 7 < int(date[5:7]):
+		days += 31
+	if 8 < int(date[5:7]):
+		days += 31
+	if 9 < int(date[5:7]):
+		days += 30
+	if 10 < int(date[5:7]):
+		days += 31
+	if 11 < int(date[5:7]):
+		days += 30
+	
+	#|print("Dias após meses:", days)
+
+	#|print("Dia: ", int(date[8:10]))
+	# Por fim, soma-se os dias da data aos dias totais:
+	days += int(date[8:10])
+
+	#|print("Dias após dias", days)
+	return days
+
+#=======================#========================================================================================================#
+
+#=#====================#=#=====================================================================================================#=#
+#=# INICIO DO PROGRAMA #=#=#===================================================================================================#=#=#
+#=#====================#=#=====================================================================================================#=#
+
+dataFrame = pd.read_csv("ks-projects-201801")
+
+print(len(dataFrame))
+dataFrame = atributoNeutro(dataFrame)
+dataFrame.to_csv("ks-projects-201801-preprocessed-incomplete")
+print(len(dataFrame))
+
+initialDay = []	# No formato de data ano-mês-dia
+finalDay = []	# No formato de data ano-mês-dia
+duration = []	# No formato convertido para dias
+dictionary = []
+
+#print(dataFrame.tail())
+
+counter = 0
+for dateBegin in dataFrame['launched']:
+	initialDay.append(convertToDays(dateBegin))
+	counter += 1
+
+counter = 0
+for dateEnd in dataFrame['deadline']:
+	finalDay.append(convertToDays(dateEnd))
+	
+	# A duração dos dias é igual a diferença entre o dia final e o inicial
+	duration.append(finalDay[counter] - initialDay[counter])
+
+	counter += 1
+
+dataFrame = dataFrame.drop(['deadline', 'launched'], 1)
+dataFrame['duration'] = duration
+
+print(dataFrame)
+
+# Cria um atributo para cada gênero que existe para poder transformá-los de classe para número, já que é um
+# problema de regressão e precisa apenas de número
+
+dataFrameAuxiliary = pd.DataFrame(index = dataFrame.index)
+
+for column, columnData in dataFrame.iteritems():   
+    if columnData.dtype == object:
+    	# Faz o 0 e 1 direitinho:
+        columnData = pd.get_dummies(columnData, prefix = column)
+        
+    dataFrameAuxiliary = dataFrameAuxiliary.join(columnData)
+    
+dataFrame = dataFrameAuxiliary
+
+dataFrame.to_csv("ks-projects-201801-preprocessed-complete")
+
+#=#====================#=#=#===================================================================================================#=#=#
+#=#====================#=#=====================================================================================================#=#
--- prediction.py
+++ prediction.py
@@ -4,157 +4,52 @@ import numpy  as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+from sklearn import preprocessing
 
-def atributoNeutro(df):
-	#Lembrando que ao recebermos um dicionário no for, ele vai printando suas palavras chave, então basta que
-	#a cada palavra chave no for, nós percorramos toda a linha
-	new_df = pd.DataFrame(df)
-	#print(new_df['LvL Adjustment'].tail())
-	for atributo in new_df:
-		#print(atributo)
-		i = 0		
-		if(atributo == 'state'):
-		#Vamos percorrer essa lista
-			aux = new_df[atributo].tolist() #Nos temos Series em nossas palavras chaves, elas são como listas, só que com outros metodos
-			for elemento in aux:
-				print(i,elemento)
-				if(elemento == "failed"):
-					new_df.drop(i, inplace = True)				
-					#Como todos os elementos são strings que serão convertidas para inteiros, colocamos ele como string tbm	
-				if(elemento == "canceled"):
-					new_df.drop(i, inplace = True)				
-					#Como todos os elementos são strings que serão convertidas para inteiros, colocamos ele como string tbm				
-				i+=1			
-	#print(new_df['LvL Adjustment'].tail())
-	return new_df
+#=#====================#=#=====================================================================================================#=#
+#=# INICIO DO PROGRAMA #=#=#===================================================================================================#=#=#
+#=#====================#=#=====================================================================================================#=#
 
-
-dataFrame = pd.read_csv("ks-projects-201801.csv")
-dataFrame = dataFrame[[ 'main_category', 'deadline', 'launched', 'state', 'country', 'usd_pledged_real', 'usd_goal_real']]
-print(len(dataFrame))
-dataFrame = atributoNeutro(dataFrame)
-print(len(dataFrame))
+dataFrame = pd.read_csv("ks-projects-dataset-altered.csv")
+dataFrame = dataFrame[[ 'main_category', 'deadline', 'launched', 'country', 'usd_goal_real', 'usd_pledged_real']]
 # Aqui é deletado todos as linhas de 40000 até o final da tabela, sendo isso feito pois com o número original
 # havia um erro de memória devida ao número muito grande de elementos
-dataFrame = dataFrame.drop(range(200001, len(dataFrame)))
-
-# DEFINIÇÂO DAS FUNÇÕES #==========================================================================================================#
-
-def convertToDays(date):
-	#|print("Ano:", int(date[0:4]))
-	# Soma-se os dias de todos os anos anteriores [ ( int(date[0:4])-1 )*365 ], contando com à soma dos dias à 
-	# mais advindos dos anos bissextos anteriores [ int( (int(date[0:4])-1)/4) ], até chegar no ano da data e 
-	# verificar quantos dias em específico ele têm indo para os meses e, por fim, os dias
-	days = 0 + ( int(date[0:4])-1 )*365 + int( (int(date[0:4])-1)/4)
-
-	#|print("Dias após anos:", days)
-
-	# Se for maior que o mês significa que o número de dias do respectivo mês deve ser
-	# acrescentado na conta dos dias, ou seja, a cada mês passado seu número de dias deve ser
-	# incrementado na contagem de dias até que chegue no mês que o atributo da data está para só assim
-	# poder passar para a parte dos dias e somá-los na conta
-	# somar com os dos meses posteriores
-	#|print("Mês:", int(date[5:7]) )
-	if 1 < int(date[5:7]):
-		days += 31
-	if 2 < int(date[5:7]):
-		# Se o ano for bissexto Fevereiro terá 29 dias
-		if (int(date[0:4])%4) == 0:
-			days += 29
-		else:
-			days += 28
-	if 3 < int(date[5:7]):
-		days += 31
-	if 4 < int(date[5:7]):
-		days += 30
-	if 5 < int(date[5:7]):
-		days += 31
-	if 6 < int(date[5:7]):
-		days += 30
-	if 7 < int(date[5:7]):
-		days += 31
-	if 8 < int(date[5:7]):
-		days += 31
-	if 9 < int(date[5:7]):
-		days += 30
-	if 10 < int(date[5:7]):
-		days += 31
-	if 11 < int(date[5:7]):
-		days += 30
-	
-	#|print("Dias após meses:", days)
-
-	#|print("Dia: ", int(date[8:10]))
-	# Por fim, soma-se os dias da data aos dias totais:
-	days += int(date[8:10])
-
-	#|print("Dias após dias", days)
-
-	return days
-
-#=======================#==========================================================================================================#
-
-initialDay = []	# No formato de data ano-mês-dia
-finalDay = []	# No formato de data ano-mês-dia
-duration = []	# No formato convertido para dias
-dictionary = []
-
-#print(dataFrame.tail())
-
-counter = 0
-for dateBegin in dataFrame['launched']:
-	initialDay.append(convertToDays(dateBegin))
-	counter += 1
-
-counter = 0
-for dateEnd in dataFrame['deadline']:
-	finalDay.append(convertToDays(dateEnd))
-	
-	# A duração dos dias é igual a diferença entre o dia final e o inicial
-	duration.append(finalDay[counter] - initialDay[counter])
-
-	counter += 1
-
-dataFrame = dataFrame.drop(['deadline', 'launched'], 1)
-dataFrame['duration'] = duration
-
-#print(dataFrame)
-
-# Cria um atributo para cada gênero que existe para poder transformá-los de classe para número, já que é um
-# problema de regressão e precisa apenas de número
-dataFrameAuxiliary = pd.DataFrame(index = dataFrame.index)
-
-for column, columnData in dataFrame.iteritems():   
-    if columnData.dtype == object:
-    	# Faz o 0 e 1 direitinho:
-        columnData = pd.get_dummies(columnData, prefix = column)
-        
-    dataFrameAuxiliary = dataFrameAuxiliary.join(columnData)
-    
-dataFrame = dataFrameAuxiliary
+#dataFrame = dataFrame.drop(range(200001, len(dataFrame)))
 
 # x e y são duas variáveis são convensões para o nome de duas variáveis que têm o seguinte significado:
 #	x é o data frame sem o atributo de saída, ou seja, as entradas 
 #	y é o data frame apenas com a saída
-x = np.array(dataFrame.drop(['duration'], 1))
-y = np.array(dataFrame['duration'])
+x = np.array(dataFrame.drop(['usd_goal_real'], axis = 1)).astype(float)
+y = np.array(dataFrame['usd_goal_real'])
+
 # train_test_split é o nosso treinamento dos sets (o split funciona como o cross validation, o cross validation
 # em si foi decreptado) 0.2 significa que tá dividindo em 5 folds (se fosse dividir em 10 seria 0.1)
 # Cria tbm o x e y de teste e de treino
-xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.7, random_state = 2)
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.5, random_state = 2)
+
 
 # Rodando todos os métodos de Machine learning:
-decisionTree = DecisionTreeRegressor(random_state = 4) # Mudar outrs parametros é aqui dentro
+decisionTree = RandomForestRegressor(random_state = 3) # Mudar outrs parametros é aqui dentro
 # Para treinar
 decisionTree.fit(xTrain, yTrain)
 # Para testar
 accuracyTree = decisionTree.score(xTest, yTest)
 
-print("\n\nAcuracia da Arvore de decisão:", accuracyTree)
+print("\n\nRandom Forest Tree:", accuracyTree)
 
-vizinhos = KNeighborsRegressor()
-vizinhos.fit(xTrain, yTrain)
-AcuraciaVizinhos = vizinhos.score(xTest, yTest)
-print("\n\nAcuracia do método de vízinhos próximos:", AcuraciaVizinhos)
+decisionTree = DecisionTreeRegressor(random_state = 4) # Mudar outrs parametros é aqui dentro
+decisionTree.fit(xTrain, yTrain)
+accuracyTree = decisionTree.score(xTest, yTest)
 
-
+print("\n\nDecision Tree:", accuracyTree)
+'''
+knn = KNeighborsRegressor()
+knn.fit(xTrain, yTrain)
+AcuraciaVizinhos = knn.score(xTest, yTest)
+print("\n\nK-Nearest Neighbors:", AcuraciaVizinhos)
+'''
+#=#====================#=#=#===================================================================================================#=#=#
+#=#====================#=#=====================================================================================================#=#
